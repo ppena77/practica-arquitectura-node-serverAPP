e1ae02d77cd2e7fc01e082a777d1b2013690a775
ppena77 <ppena77@alumno.uned.es>
2023-09-27 18:15:52 +0200

    Created temp route file. Receiving data from Sensor APP

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 // ROUTE SCRIPTS
 
@@ -21,6 +22,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/temp.js
+++ routes/temp.js
@@ -1,14 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+let router = express.Router();
 
-/* GET TEMPERATURES */
-router.get('/', function(req, res, next) {
+// * EXPRESS MIDDLEWARE
+
+// * GET TEMPERATURES
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/* POST TEMPERATURES (Registro temperatura en MongoDB) */
-router.get('/reg', (req, res) => {
-    res.send('End point funionando')
+//* POST TEMPERATURES (Registro temperatura en MongoDB)
+router.post('/reg', (req, res) => {
+
+  /* Validación para comprobar que los datos recibidos vienen en Json */
+  if (req.headers['content-type'] === 'application/json') {
+
+    let data = req.body; // Guardamos datos recibidos
+    console.log(data);
+
+  } else {
+
+    console.log('Recieved request in /temp/reg url is not a Json object');
+    res.send('Recieved request in /temp/reg is not a Json object');
+
+  }    
 });
 
 module.exports = router;
