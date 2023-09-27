80935a5d26756790553dcc61ae993c892947f70c
ppena77 <ppena77@alumno.uned.es>
2023-09-27 19:30:44 +0200

    Added connection to MongoDB and insertion in test collection of sensor data

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // * Importamos Body Parser para poder leer los datos del "body" de los request que lleguen a las rutas definidas.
+const mongoose = require('mongoose'); // * Importamos Mongoose
 
 // ROUTE SCRIPTS
 
@@ -20,17 +21,35 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// MIDDLEWARE
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // * Llamamos al middleware Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// * MONGOOSE CONNECTION
+
+const dev_db_url = 'mongodb://127.0.0.1:27017/test';
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url; // Indicamos que para conectarnos a MongoDB, podemos usar la variable de entorno que queramos (no hemos visto aún variables de entorno, pero lo dejo para testar la aplicación) o, la variable dev_db_url previamente definida.
+
+mongoose.connect(mongoDB); // Hacemos la llamada a MongoDB con la ruta facilitada en la variable mongoDB
+
+// mongoose.Promise = global.Promise; // ? Hacemos que la instancia de mongoose se ejecute como una promesa ?? Deprecado ??
+
+const db = mongoose.connection; // ? Cargamos el objeto connection de moongose en una variable para poder escuchar los estados de la conexion ??
+
+db.on('error', console.error.bind(console, 'MongoDB connection error')); // ? Listener del estado de la conexión para cuando se lanza un error y manejar dicho error ?
+
+
 // ROUTING
 
-app.use('/', indexRouter);
-app.use('/temp', tempRouter);
+app.use('/', indexRouter); // * Ruta por defecto, sin funcionalidad ninguna
+app.use('/temp', tempRouter); // * Ruta de trabajo para la práctica
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/temp.js
+++ routes/temp.js
@@ -1,27 +1,39 @@
 const express = require('express');
-let router = express.Router();
+const router = express.Router();
 
-// * EXPRESS MIDDLEWARE
+// Importamos modelo mongoDB "SensorData" 
+const SensorData = require('../models/sensorData');
 
-// * GET TEMPERATURES
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+// * GET TEMPERATURES (Para extraer la temperatura máxima, mínima y la media)
+
+router.get('/', (req, res) => {
+  SensorData.find({})
+    .then( result => {
+      res.json( result )
+    })
+    .catch( err => console.log(err) )
 });
 
 //* POST TEMPERATURES (Registro temperatura en MongoDB)
+
 router.post('/reg', (req, res) => {
 
   /* Validación para comprobar que los datos recibidos vienen en Json */
   if (req.headers['content-type'] === 'application/json') {
 
-    let data = req.body; // Guardamos datos recibidos
-    console.log(data);
+    const data = req.body; // Guardamos datos recibidos
 
+    // Apuntamos a la colección de mongoDB "SensorData" y mandamos a inyectar los datos como un nuevo Documento
+    SensorData.create(data) 
+      .then( result => { 
+        res.json(result) // Si todo va bien, mandamos el resultado que nos devuelve el método "create" de mongoose como respuestas
+      })
+      .catch( err => console.log(err) ) // Si no va bien, imprimimos el error
+    
   } else {
-
+    /* Si los datos que llegan no vienen como json, mandamos respuesta a Sensor App e imprimimos en consola */
     console.log('Recieved request in /temp/reg url is not a Json object');
     res.send('Recieved request in /temp/reg is not a Json object');
-
   }    
 });
 
